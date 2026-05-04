5d9c8efef4d4dcb641183ce6ca4848d61a5a8834
faizfalahh <faizfalah38@gmail.com>
2026-05-04 17:03:53 +0800

    Menambahkan library iostream dan string, Membuat class Mahasiswa dengan akses modifier private dan public, Mendeklarasikan variabel static nim (private), Menambahkan atribut id dan nama (public), Mendeklarasikan method setID dan printAll

---
+++ staticfunction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string> //menyertakan library string
+using namespace std;
+
+class Mahasiswa {
+
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+   
