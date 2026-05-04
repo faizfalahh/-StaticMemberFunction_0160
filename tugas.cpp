b91f62f3776886122aeaf34e9271e8e8146996ce
faizfalahh <faizfalah38@gmail.com>
2026-05-04 17:49:02 +0800

    menambahkan constructor default pada class Produk

--- tugas.cpp
+++ tugas.cpp
@@ -7,3 +7,11 @@ class Produk
 private:
     int id_Produk; // menambahkan variabel id produk, nama produk
     string nama_Produk;
+
+    public:
+    Produk()
+    { // membuat construction tanpa parameter
+        id_Produk = 0;
+        nama_Produk = "tidak ada";
+    }
+
